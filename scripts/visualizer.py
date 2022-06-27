9866adb6bb095b541d54722981c667248bf0bff7
Ammon21 <ammonb6@gmail.com>
2022-06-28 00:57:31 +0300

    Visualizing Data

--- scripts/visualizer.py
+++ scripts/visualizer.py
@@ -4,9 +4,9 @@ import seaborn as sns
 
 
 def plot_count(df:pd.DataFrame, column:str) -> None:
-    plt.figure(figsize=(9, 5))
+    plt.figure(figsize=(6, 6))
     sns.countplot(data=df, x=column, facecolor=(0, 0, 0, 0), linewidth=5,  edgecolor=sns.color_palette("pastel", 3))
-    plt.title(f'Distribution of {column}', size=20, fontweight='bold')
+    plt.title(f'Distribution of {column}', size=18, fontweight='bold')
     plt.xlabel(f'{column}', fontsize=17)
     plt.ylabel("Count", fontsize=17)
     plt.show()
